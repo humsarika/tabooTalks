78410389e21a1d494791f32d02b15504385423f4
Mitali2700 <201B155@juetguna.in>
2022-10-02 01:01:08 +0530

    js code started

---
+++ taboo.js
@@ -0,0 +1,7 @@
+const menuBtn = document.querySelector('.menu-btn');
+const navlinks = document.querySelector('.nav-links');
+menuBtn.addEventListener('click',()=>{
+    navlinks.classList.toggle('mobile-menu')
+});
+
+ 
