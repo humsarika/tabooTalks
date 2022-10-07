dd0632fb8deb45f63aa8fe4d32883299ba017e55
Mitali2700 <201B155@juetguna.in>
2022-10-08 00:10:55 +0530

    facing error

--- tt-website/src/App.js
+++ tt-website/src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BroweserRouter as Router, Switch, Route} from 'react-router-dom';
+import { Switch ,Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <>
-      <Router>
+      <Route>
         <Navbar />
-        {/* <Switch>
+        <Switch>
           <Route path='/' exact />
-        </Switch> */}
-      </Router>
+        </Switch> 
+      </Route>
     
     </>
     
